831ebd8a322bc5e3273ddb4576d746b8183e3a67
SusanoNoMikoto <Susan0N0Mik0t0@outlook.com>
2025-06-07 20:42:44 +0300

    test launch tg bot

---
+++ internal/db/db.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Postgres struct {
+	Pool *pgxpool.Pool
+}
+
+func NewPostgres(ctx context.Context, connString string) (*Postgres, error) {
+	pool, err := pgxpool.New(ctx, connString)
+	if err != nil {
+		return nil, err
+	}
+	if err = pool.Ping(ctx); err != nil {
+		return nil, err
+	}
+	return &Postgres{Pool: pool}, nil
+}

67e1ae4f0e5ddf4df5510a720ebed47c8ef585e2
SusanoNoMikoto <Susan0N0Mik0t0@outlook.com>
2025-06-08 08:10:00 +0300

    tg_bot Impl

--- cmd/pr_chat_bot/main.go
+++ cmd/pr_chat_bot/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"os"
-
-	"github.com/troxanna/pr-chat-backend/internal/config"
 	"github.com/troxanna/pr-chat-backend/internal/application"
+	"github.com/troxanna/pr-chat-backend/internal/bot"
+	"github.com/troxanna/pr-chat-backend/internal/config"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -15,9 +18,33 @@ func main() {
 		os.Exit(1)
 	}
 
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
 
-	if err = application.New("pr", cfg).Run(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	myBot, err := bot.NewBot("7661896995:AAE5P63Q-bBNvA1dZDmFrIeg31xtsOHkqvY")
+	if err != nil {
+		fmt.Println("Ошибка создания бота:", err)
+		return
 	}
+
+	go func() {
+		if err := myBot.Start(ctx); err != nil {
+			fmt.Println("Ошибка работы бота:", err)
+		}
+		fmt.Println("Бот остановлен")
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Получен сигнал завершения, завершаем main")
+			return
+		default:
+			if err = application.New("pr", cfg).Run(); err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+		}
+	}
+
 }
